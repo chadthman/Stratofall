3bbe49328769e1c51736200935b4c2788da61078
Chad Marmon <chadthman@gmail.com>
2013-10-16 21:16:50 -0600

    Updated Classes to working code

--- Stratofall/src/objects/LightningCloud.java
+++ Stratofall/src/objects/LightningCloud.java
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 package objects;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
@@ -34,54 +34,10 @@ public class LightningCloud extends Cloud
 		min_reset_time = 3 * 1000000000f; //3 seconds
 		setResetTime();
 		
-		VELOCITY_Y = 2.5f; //moves half the speed of a normal cloud
-	}
-
-	@Override
-	public void checkCollisions()
-	{
-		
-		if(getLocation().overlaps(player.getLocation())) //if collides with player
-		{
-			//player.setLocation(new Vector2(player.getLocation().x, 1100)); //visible collision check
-		}
-	}
-}
-=======
-package objects;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.me.stratofall.Player;
-import com.me.stratofall.Stratofall;
-
-public class LightningCloud extends Cloud
-{
-	
-	public LightningCloud(Player p)
-	{
-		player = p;
-		cloudImage = new TextureRegion(atlas.findRegion("cloud_lightning"));
-		cloud = new Rectangle();
-		cloud.width = cloudImage.getRegionWidth();
-		cloud.height = cloudImage.getRegionHeight();
-		
-		//load sound effect
-		//effectSound = Gdx.audio.newSound(Gdx.files.internal("sounds/effect/sound_lightning.wav")); //this sound is actually for lightning clouds, just temporary
-		effectSound = Stratofall.assets.get("sounds/effect/sound_lightning.wav", Sound.class);
-		
-		//give a random locaton on the x axis
-		cloud.x = random.nextInt(Stratofall.WIDTH) - (cloud.width)/2;
-		cloud.y = -cloud.height;
-		
-		//reset times
-		max_reset_time = 15 * 1000000000f; //15 seconds
-		min_reset_time = 3 * 1000000000f; //3 seconds
-		setResetTime();
+		//get the rain effect
+		effect = new ParticleEffect();
+		effect.load(Gdx.files.internal("particles/effects/cloud_rain.p"), Gdx.files.internal("particles/effects"));
+		effect.start(); //start the effect
 		
 		VELOCITY_Y = 2.5f; //moves half the speed of a normal cloud
 	}
@@ -89,11 +45,10 @@ public class LightningCloud extends Cloud
 	@Override
 	public void checkCollisions()
 	{
-		
 		if(getLocation().overlaps(player.getLocation())) //if collides with player
 		{
 			//player.setLocation(new Vector2(player.getLocation().x, 1100)); //visible collision check
 		}
 	}
 }
->>>>>>> 85753e1268beba5402fd8440d0d70f05e2040eba
+

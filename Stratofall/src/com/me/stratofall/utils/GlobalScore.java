9368930f7e03a0188a8b0d23d9f638f14465fa23
Chad Marmon <chadthman@gmail.com>
2014-04-07 19:48:47 -0600

    Commented out more Stackmob

--- Stratofall/src/com/me/stratofall/utils/GlobalScore.java
+++ Stratofall/src/com/me/stratofall/utils/GlobalScore.java
@@ -2,20 +2,17 @@ package com.me.stratofall.utils;
 
 import java.util.ArrayList;
 
-import com.stackmob.sdk.model.StackMobModel;
-
-
 /*
  * Stackmob has been deprecated so this will change to something else if it fully becomes implemented
  *
  */
-public class GlobalScore extends StackMobModel {
+public class GlobalScore {//extends StackMobModel {
 
 	private ArrayList<Score> globalScore;
 	
 	public GlobalScore(ArrayList<Score> globalScore)
 	{
-		super(GlobalScore.class);
+		//super(GlobalScore.class);
 		this.globalScore = globalScore;
 	}
 	
